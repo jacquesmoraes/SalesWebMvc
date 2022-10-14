125049f8278380876a27bdd7bb79ef00041bba9f
jacquesmoraes <jacquesbmoraes@hotmail.com>
2022-10-14 20:11:02 -0300

    Sellers controller

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellersController(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Seller.ToListAsync());
+        }
+    }
+}
