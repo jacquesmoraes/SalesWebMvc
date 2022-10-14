60499095d1973d8ba6e1fee70e68366cbbb3a73b
jacquesmoraes <jacquesbmoraes@hotmail.com>
2022-10-13 21:56:10 -0300

    other entities and second migration

---
+++ SalesWebMvc/Models/Enums/SalesStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models.Enums
+{
+    public enum SalesStatus : int
+    {
+        pending = 0,
+        billed = 1,
+        canceled = 2
+    }
+}
