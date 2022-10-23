592f0001388c2416413c35bb8a12c412ef5c3ac3
jacquesmoraes <jacquesbmoraes@hotmail.com>
2022-10-22 22:11:01 -0300

    Implementing group search

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -41,9 +41,20 @@ namespace SalesWebMvc.Controllers
             return View(result);
         }
 
-        public IActionResult GroupingSearch()
+        public async Task<IActionResult> GroupingSearch(DateTime? min, DateTime? max)
         {
-            return View();
+            if (!min.HasValue)
+            {
+                min = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!max.HasValue)
+            {
+                max = DateTime.Now;
+            }
+            ViewData["min"] = min.Value.ToString("yyyy-MM-dd");
+            ViewData["max"] = max.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordService.FindByDateGroupingAsync(min, max);
+            return View(result);
         }
     }
 }
--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -40,6 +40,23 @@ namespace SalesWebMvc.Services
                 .Where(x => x.Status == SalesStatus.Billed)
                 .OrderByDescending(x => x.Date).ToListAsync();
         }
+        public async Task<List<IGrouping<Department,SalesRecord>>> FindByDateGroupingAsync(DateTime? min, DateTime? max)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (min.HasValue)
+            {
+                result = result.Where(x => x.Date >= min.Value);
+            }
+            if (max.HasValue)
+            {
+                result = result.Where(x => x.Date <= max.Value);
+            }
 
+            var data =  await result.Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+            return data.GroupBy(x => x.Seller.Department).ToList();
+        }
     }
 }
